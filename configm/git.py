7ad02165f4ea21b7ccb9b83b5375949950a52455
Dan Kilman <dankilman@gmail.com>
2015-09-24 18:30:48 +0300

    better checking during git clone

--- configm/git.py
+++ configm/git.py
@@ -13,7 +13,19 @@ git = bake(sh.git)
 
 def clone(source, target, logger):
     if target.exists():
-        logger.info('Not cloning {0} as it already exists in {1}'
-                    .format(source, target))
+        with target:
+            try:
+                remote = sh.git.config('remote.origin.url').stdout.strip()
+                if remote == source:
+                    logger.info('Not cloning {0} as it already exists in {1}'
+                                .format(source, target))
+                else:
+                    logger.warn('Not cloning {0} as it already exists in {1}. '
+                                'Note that the current remote.origin is {2}'
+                                .format(source, target, remote))
+            except sh.ErrorReturnCode:
+                logger.warn('Not cloning {0} as it already exists in {1}. '
+                            'Note that current target does not point to a '
+                            'valid git repository'.format(source, target))
     else:
         git.clone(source, target).wait()
